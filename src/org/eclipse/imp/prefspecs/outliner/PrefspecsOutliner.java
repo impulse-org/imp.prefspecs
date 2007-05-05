8097e01cdc5027dddb237217d14d54091923460d
Stanley M. Sutton <suttons@us.ibm.com>
2007-05-05 16:15:04 +0000

    Adapted to new utility routines to simplify implementation of visitors.

--- src/org/eclipse/imp/prefspecs/outliner/PrefspecsOutliner.java
+++ src/org/eclipse/imp/prefspecs/outliner/PrefspecsOutliner.java
@@ -1,10 +1,31 @@
 package prefspecs.safari.outliner;
 
 
-import org.eclipse.uide.defaults.*;	
-import org.eclipse.uide.parser.IParseController;
+import org.eclipse.uide.defaults.OutlinerBase;
+import org.eclipse.uide.editor.IOutlineImage;
 
-import prefspecs.safari.parser.Ast.*;
+import prefspecs.safari.documentationProvider.PrefspecsNodeDocumentationProvider;
+import prefspecs.safari.parser.Ast.ASTNode;
+import prefspecs.safari.parser.Ast.AbstractVisitor;
+import prefspecs.safari.parser.Ast.booleanFieldSpec;
+import prefspecs.safari.parser.Ast.comboFieldSpec;
+import prefspecs.safari.parser.Ast.conditionalSpec0;
+import prefspecs.safari.parser.Ast.conditionalSpec1;
+import prefspecs.safari.parser.Ast.conditionalsSpec;
+import prefspecs.safari.parser.Ast.configurationTabSpec;
+import prefspecs.safari.parser.Ast.customRule;
+import prefspecs.safari.parser.Ast.customSpec;
+import prefspecs.safari.parser.Ast.defaultTabSpec;
+import prefspecs.safari.parser.Ast.dirListFieldSpec;
+import prefspecs.safari.parser.Ast.fieldsSpec;
+import prefspecs.safari.parser.Ast.fileFieldSpec;
+import prefspecs.safari.parser.Ast.instanceTabSpec;
+import prefspecs.safari.parser.Ast.intFieldSpec;
+import prefspecs.safari.parser.Ast.pageSpec;
+import prefspecs.safari.parser.Ast.projectTabSpec;
+import prefspecs.safari.parser.Ast.radioFieldSpec;
+import prefspecs.safari.parser.Ast.stringFieldSpec;
+import prefspecs.safari.parser.Ast.tabsSpec;
 
 /**
  * This file provides a skeletal implementation of the language-dependent aspects
@@ -18,6 +39,12 @@ import prefspecs.safari.parser.Ast.*;
  */
 public class PrefspecsOutliner extends OutlinerBase
 {
+
+	
+	// Why not static?
+	PrefspecsNodeDocumentationProvider docProvider = new PrefspecsNodeDocumentationProvider();
+	
+	
 	/*
 	 * A visitor for ASTs.  Its purpose is to create outline-tree items
 	 * for AST node types that are to appear in the outline view.
@@ -58,9 +85,11 @@ public class PrefspecsOutliner extends OutlinerBase
 		
 		// Tab specs
 		
-		
 		public boolean visit(tabsSpec n) {
 			pushSubItem("Tabs", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
@@ -68,35 +97,73 @@ public class PrefspecsOutliner extends OutlinerBase
 			popSubItem();
 		}
 	
-		
+		 boolean inDefaultTabSpec = false;
+
 		public boolean visit(defaultTabSpec n) {
-			addSubItem("Default tab", n);
+			//addSubItem("Default tab", n);
+			pushSubItem("Default tab", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
+			inDefaultTabSpec = true;
 			return true;
+		}
+		
+		public void endVisit(defaultTabSpec n) {
+			inDefaultTabSpec = false;
+			popSubItem();
 		}
 		
 		
 		public boolean visit(configurationTabSpec n) {
-			addSubItem("Configuration tab", n);
+			//addSubItem("Configuration tab", n);
+			//pushSubItem(docProvider.getDocumentation(n, editor.getParseController()), n);
+			pushSubItem("Configuration tab", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(configurationTabSpec n) {
+			popSubItem();
+		}
 		
 		public boolean visit(instanceTabSpec n) {
-			addSubItem("Instance tab", n);
+			//addSubItem("Instance tab", n);
+			//pushSubItem(docProvider.getDocumentation(n, editor.getParseController()), n);
+			pushSubItem("Instance tab", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(instanceTabSpec n) {
+			popSubItem();
+		}
 		
 		public boolean visit(projectTabSpec n) {
-			addSubItem("Project tab" , n);
+			//addSubItem("Project tab" , n);
+			//pushSubItem(docProvider.getDocumentation(n, editor.getParseController()), n);
+			pushSubItem("Project tab" , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(projectTabSpec n) {
+			popSubItem();
+		}
 		
 		// Field specs
 		
 		public boolean visit(fieldsSpec n) {
 			pushSubItem("Fields", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 
@@ -106,51 +173,111 @@ public class PrefspecsOutliner extends OutlinerBase
 		
 		
 		public boolean visit(booleanFieldSpec n) {
-			addSubItem(((booleanFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((booleanFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((booleanFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
+		}
+
+		public void endVisit(booleanFieldSpec n) {
+			popSubItem();
 		}
 		
 		
 		public boolean visit(comboFieldSpec n) {
-			addSubItem(((comboFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((comboFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((comboFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
+		}
+		
+		
+		public void endVisit(comboFieldSpec n) {
+			popSubItem();
 		}
 		
 		
 		public boolean visit(dirListFieldSpec n) {
-			addSubItem(((dirListFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((dirListFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((dirListFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
+		}
+		
+		public void endVisit(dirListFieldSpec n) {
+			popSubItem();
 		}
 		
 		
 		public boolean visit(fileFieldSpec n) {
-			addSubItem(((fileFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((fileFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((fileFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
+		}
+		
+		public void endVisit(fileFieldSpec n) {
+			popSubItem();
 		}
 		
 		
 		public boolean visit(intFieldSpec n) {
-			addSubItem(((intFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((intFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((intFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
-		
+	
+		public void endVisit(intFieldSpec n) {
+			popSubItem();
+		}
+
 		
 		public boolean visit(radioFieldSpec n) {
-			addSubItem(((radioFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((radioFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((radioFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(radioFieldSpec n) {
+			popSubItem();
+		}
+
 		
 		public boolean visit(stringFieldSpec n) {
-			addSubItem(((stringFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(((stringFieldSpec)n).getidentifier().toString() , n);
+			pushSubItem(((stringFieldSpec)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(stringFieldSpec n) {
+			popSubItem();
+		}
+
 		
 		// Custom
 		
 		public boolean visit(customSpec n) {
 			pushSubItem("Custom" , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
@@ -159,31 +286,61 @@ public class PrefspecsOutliner extends OutlinerBase
 		}
 		
 		public boolean visit(customRule n) {
-			addSubItem(n.gettab().toString() + "." + n.getidentifier().toString(), n);
+			//addSubItem(n.gettab().toString() + "." + n.getidentifier().toString(), n);
+			pushSubItem(((customRule)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 		
+		public void endVisit(customRule n) {
+			popSubItem();
+		}
+
 		
 		// Conditionals
 		
 		public boolean visit(conditionalsSpec n) {
 			pushSubItem("Conditionals", n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
 
 		public void endVisit(conditionalsSpec n) {
 			popSubItem();
 		}
+	
 		
 		public boolean visit(conditionalSpec0 n) {
-			addSubItem(n.getidentifier().toString() + " with " + n.getidentifier3().toString(), n);
+			//addSubItem(n.getidentifier().toString() + " with " + n.getidentifier3().toString(), n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
+	
+		public void endVisit(conditionalSpec0 n) {
+			popSubItem();
+		}
+
 		
 		public boolean visit(conditionalSpec1 n) {
-			addSubItem(n.getidentifier().toString() + " against " + n.getidentifier3().toString(), n);
+			//addSubItem(n.getidentifier().toString() + " against " + n.getidentifier3().toString(), n);
+			pushSubItem(((conditionalSpec1)n).getidentifier().toString() , n);
+			//addSubItem(docProvider.getDocumentation(n, editor.getParseController()), null);
+			createSubItem(docProvider.getDocumentation(n, editor.getParseController()), null, 
+							PrefspecsDocOutlineImage.getPrefspecsDocOutlineImage());
 			return true;
 		}
+
+		public void endVisit(conditionalSpec1 n) {
+			popSubItem();
+		}
+
+		
 		
     }
 
