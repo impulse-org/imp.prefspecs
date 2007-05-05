31df7d0c338a96c1d4d8f42500544ec9d2f066f8
Stanley M. Sutton <suttons@us.ibm.com>
2007-05-05 16:16:08 +0000

    Adaptations to new way of handling outline images.

--- src/org/eclipse/imp/prefspecs/outliner/PrefspecsDocOutlineImage.java
+++ src/org/eclipse/imp/prefspecs/outliner/PrefspecsDocOutlineImage.java
@@ -8,25 +8,27 @@ import org.eclipse.uide.editor.IOutlineImage;
 
 //public class PrefspecsImages {
 	
-public class PrefspecsImages implements IOutlineImage
+public class PrefspecsDocOutlineImage implements IOutlineImage
 {
-	private PrefspecsImages() { }
+	private PrefspecsDocOutlineImage() { }
 	
-	private static PrefspecsImages image = null;
+	private static PrefspecsDocOutlineImage image = null;
 	
-	public static PrefspecsImages getPrefspecsImages() {
+	public static PrefspecsDocOutlineImage getPrefspecsDocOutlineImage() {
 		if (image == null) {
-			image = new PrefspecsImages();
+			image = new PrefspecsDocOutlineImage();
 		}
 		return image;
 	}
-
+	
 	public static final String IMAGE_ROOT= "icons";
+	
+	//public static ImageDescriptor OUTLINE_ITEM_DESC= AbstractUIPlugin.imageDescriptorFromPlugin("prefspecs", IMAGE_ROOT + "/doc_outline_item.gif");
+	public static ImageDescriptor OUTLINE_ITEM_DESC= AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.uide.prefspecs", IMAGE_ROOT + "/doc_outline_item.gif");
 
-	public static ImageDescriptor OUTLINE_ITEM_DESC= AbstractUIPlugin.imageDescriptorFromPlugin("prefspecs", IMAGE_ROOT + "/outline_item.gif");
 	public static Image OUTLINE_ITEM_IMAGE= OUTLINE_ITEM_DESC.createImage();
 
-
+	
 	public String getImageRoot() {
 		return IMAGE_ROOT;
 	}
@@ -38,5 +40,6 @@ public class PrefspecsImages implements IOutlineImage
 	public Image getOutlineItemImage() {
 		return OUTLINE_ITEM_IMAGE;
 	}
+
 	
 }
