ced04f79932d1d9d4b79699ee55ec7f204c93f8f
Stanley M. Sutton <suttons@us.ibm.com>
2008-01-31 13:44:09 +0000

    Generally updated version based on most recent template, which has many differences from the one used to create this class initially.

--- src/org/eclipse/imp/prefspecs/PrefspecsPlugin.java
+++ src/org/eclipse/imp/prefspecs/PrefspecsPlugin.java
@@ -5,39 +5,69 @@
  */
 package org.eclipse.imp.prefspecs;
 
-//import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.imp.preferences.PreferencesService;
 import org.eclipse.imp.runtime.PluginBase;
 import org.osgi.framework.BundleContext;
 
+/*
+ * SMS 27 Mar 2007:  Deleted creation of preferences cache (now obsolete)
+ * SMS 28 Mar 2007:
+ * 	- Plugin class name now totally parameterized
+ *  - Plugin package made a separate parameter
+ * SMS 19 Jun 2007:
+ * 	- Added kLanguageName (may be used by later updates to the template)
+ * 	- Added field and method related to new preferences service; deleted
+ *	  code for initializing preference store from start(..) method
+ */
+
 public class PrefspecsPlugin extends PluginBase {
-    public static final String kPluginID= "org.eclipse.imp.prefspecs";
 
-    /**
-     * The unique instance of this plugin class
-     */
-    protected static PrefspecsPlugin sPlugin;
+	public static final String kPluginID = "org.eclipse.imp.prefspecs";
 
-    public static PrefspecsPlugin getInstance() {
-        return sPlugin;
-    }
+	public static final String kLanguageName = "prefspecs";
 
-    public PrefspecsPlugin() {
-	super();
-	sPlugin= this;
-    }
+	/**
+	 * The unique instance of this plugin class
+	 */
+	protected static PrefspecsPlugin sPlugin;
 
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
+	public static PrefspecsPlugin getInstance() {
+		// SMS 11 Jul 2007
+		// Added conditional call to constructor in case the plugin
+		// class has not been auto-started
+		if (sPlugin == null)
+			new PrefspecsPlugin();
+		return sPlugin;
+	}
 
-	// Initialize the Preferences fields with the preference store data.
-//	IPreferenceStore prefStore= getPreferenceStore();
+	public PrefspecsPlugin() {
+		super();
+		sPlugin = this;
+	}
 
-//	PrefspecsPreferenceCache.builderEmitMessages= prefStore.getBoolean(PrefspecsPreferenceConstants.P_EMIT_MESSAGES);
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
 
-//	fEmitInfoMessages= PrefspecsPreferenceCache.builderEmitMessages;
-    }
+	}
 
-    public String getID() {
-	return kPluginID;
-    }
+	public String getID() {
+		return kPluginID;
+	}
+
+	protected static PreferencesService preferencesService = null;
+
+	public static PreferencesService getPreferencesService() {
+		if (preferencesService == null) {
+			preferencesService = new PreferencesService(ResourcesPlugin
+					.getWorkspace().getRoot().getProject());
+			preferencesService.setLanguageName(kLanguageName);
+			// TODO:  When some actual preferences are created, put
+			// a call to the preferences initializer here
+			// (The IMP New Preferences Support wizard creates such
+			// an initializer.)
+
+		}
+		return preferencesService;
+	}
 }
