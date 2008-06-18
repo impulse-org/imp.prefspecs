575020bf860b45790c8d178cb97bddb628ad89e9
Stanley M. Sutton <suttons@us.ibm.com>
2008-06-18 17:00:07 +0000

    New interface generated with label provider.

---
+++ src/org/eclipse/imp/prefspecs/IPrefspecsResources.java
@@ -0,0 +1,19 @@
+package org.eclipse.imp.prefspecs;
+
+public interface IPrefspecsResources {
+
+	// Definitions for label provider
+
+	public static final String PREFSPECS_DEFAULT_IMAGE = "prefspecs_default_image";
+
+	public static final String PREFSPECS_DEFAULT_OUTLINE_ITEM = "prefspecs_default_outline_item";
+
+	public static final String PREFSPECS_FILE = "prefspecs_file";
+
+	public static final String PREFSPECS_FILE_WARNING = "prefspecs_file_warning";
+
+	public static final String PREFSPECS_FILE_ERROR = "prefspecs_file_error";
+
+	// Definitions for label provider end
+
+}
