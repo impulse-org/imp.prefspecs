b303eae67adbdccaa9d7027f40d77649e1c67dd5
Stanley M. Sutton <suttons@us.ibm.com>
2008-10-19 18:17:19 +0000

    Added copyright notice--EPL, 2008, IBM, suttons contributor.

--- src/org/eclipse/imp/prefspecs/treeModelBuilder/PrefspecsLabelProvider.java
+++ src/org/eclipse/imp/prefspecs/treeModelBuilder/PrefspecsLabelProvider.java
@@ -1,11 +1,15 @@
-/*
- * (C) Copyright IBM Corporation 2007
- * 
- * This file is part of the Eclipse IMP.
- */
-/*
- * Created on Jul 7, 2006
- */
+/*******************************************************************************
+* Copyright (c) 2008 IBM Corporation.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*    Stan Sutton (suttons@us.ibm.com) - initial API and implementation
+
+*******************************************************************************/
+
 package org.eclipse.imp.prefspecs.treeModelBuilder;
 
 import java.util.HashSet;
