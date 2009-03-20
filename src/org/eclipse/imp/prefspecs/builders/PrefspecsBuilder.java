c39187b7bf747495c9b09acf54c2f35fea34b4bf
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-20 02:31:21 +0000

    Now uses new API on BuilderBase regarding consoles, and directs diagnostic msgs there instead of System.out or System.err.

--- src/org/eclipse/imp/prefspecs/builders/PrefspecsBuilder.java
+++ src/org/eclipse/imp/prefspecs/builders/PrefspecsBuilder.java
@@ -7,34 +7,27 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
 package org.eclipse.imp.prefspecs.builders;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.builder.BuilderBase;
-import org.eclipse.imp.builder.BuilderUtils;
-import org.eclipse.imp.builder.MarkerCreator;
 import org.eclipse.imp.language.Language;
 import org.eclipse.imp.language.LanguageRegistry;
-import org.eclipse.imp.model.ISourceProject;
-import org.eclipse.imp.model.ModelFactory;
-import org.eclipse.imp.model.ModelFactory.ModelException;
-import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.prefspecs.PrefspecsPlugin;
 import org.eclipse.imp.prefspecs.compiler.PrefspecsCompiler;
-import org.eclipse.imp.prefspecs.parser.PrefspecsParseController;
 import org.eclipse.imp.runtime.PluginBase;
 
 /**
  * @author
  */
 public class PrefspecsBuilder extends BuilderBase {
+    protected static final String PREFSPECS_CONSOLE_NAME= "Pref Specs Builder";
+
     /**
      * Extension ID of the Prefspecs builder. Must match the ID in the corresponding
      * extension definition in plugin.xml.
@@ -42,30 +35,31 @@ public class PrefspecsBuilder extends BuilderBase {
      */
 	// SMS 28 Mar 2007:  Make plugin class name totally parameterized
 	public static final String BUILDER_ID= PrefspecsPlugin.kPluginID + ".builder";
+
 	// SMS 28 Mar 2007:  Make problem id parameterized (rather than just ".problem") so that
-	// it can be given a builde-specific value (not simply composed here using the builder id
+	// it can be given a build-specific value (not simply composed here using the builder id
 	// because the problem id is also needed in ExtensionPointEnabler for adding the marker
 	// extension to the plugin.xml file)
     public static final String PROBLEM_MARKER_ID= PrefspecsPlugin.kPluginID + ".problem";
     
-    // SMS 11 May 2006
-    public static final String LANGUAGE_NAME = "prefspecs";
-    public static final Language LANGUAGE = LanguageRegistry.findLanguage(LANGUAGE_NAME);
+    public static final Language LANGUAGE = LanguageRegistry.findLanguage(PrefspecsPlugin.kLanguageID);
 
-
+    @Override
     protected PluginBase getPlugin() {
-        //return PrefspecsPlugin.getInstance();
         return PrefspecsPlugin.getInstance();
     }
 
+    @Override
     protected String getErrorMarkerID() {
         return PROBLEM_MARKER_ID;
     }
 
+    @Override
     protected String getWarningMarkerID() {
         return PROBLEM_MARKER_ID;
     }
 
+    @Override
     protected String getInfoMarkerID() {
         return PROBLEM_MARKER_ID;
     }
@@ -74,6 +68,7 @@ public class PrefspecsBuilder extends BuilderBase {
     // SMS 11 May 2006
     // Incorporated realistic handling of filename extensions
     // using information recorded in the language registry
+    @Override
     protected boolean isSourceFile(IFile file) {
         IPath path= file.getRawLocation();
         if (path == null) return false;
@@ -84,45 +79,46 @@ public class PrefspecsBuilder extends BuilderBase {
         return LANGUAGE.hasExtension(path.getFileExtension());
     }
 
-
     /**
      * @return true iff the given file is a source file that this builder should scan
      * for dependencies, but not compile as a top-level compilation unit.<br>
      * <code>isNonRootSourceFile()</code> and <code>isSourceFile()</code> should never
      * return true for the same file.
      */
-    protected boolean isNonRootSourceFile(IFile resource)
-    {
+    @Override
+    protected boolean isNonRootSourceFile(IFile resource) {
         // Nothing to do - the prefspecs language does not define anything like "include" files.
         return false;
+    }
+
+    @Override
+    protected boolean isOutputFolder(IResource resource) {
+        return resource.getFullPath().lastSegment().equals("bin");
     }
 
     /**
      * Collects compilation-unit dependencies for the given file, and records
      * them via calls to <code>fDependency.addDependency()</code>.
      */
-    protected void collectDependencies(IFile file)
-    {
+    @Override
+    protected void collectDependencies(IFile file) {
         // Nothing to do - the prefspecs language does not support inter-file references.
         return;
     }
 
-    
-    protected boolean isOutputFolder(IResource resource) {
-        return resource.getFullPath().lastSegment().equals("bin");
+    @Override
+    protected String getConsoleName() {
+        return PREFSPECS_CONSOLE_NAME;
     }
 
-    
+    @Override
     protected void compile(final IFile file, IProgressMonitor monitor) {
         try {
-            // START_HERE
-            System.out.println("Builder.compile with file = " + file.getName());
-            PrefspecsCompiler compiler= new PrefspecsCompiler(PROBLEM_MARKER_ID);
+            getConsoleStream().println("Builder.compile with file = " + file.getName());
+
+            PrefspecsCompiler compiler= new PrefspecsCompiler(PROBLEM_MARKER_ID, getConsoleStream());
+
             compiler.compile(file, monitor);
-            // Here we provide a substitute for the compile method that simply
-            // runs the parser in place of the compiler but creates problem
-            // markers for errors that will show up in the problems view
-            //runParserForCompiler(file, monitor);
 
             doRefresh(file.getParent());
         } catch (Exception e) {
@@ -131,43 +127,4 @@ public class PrefspecsBuilder extends BuilderBase {
             e.printStackTrace();
         }
     }
-
-    protected void runParserForCompiler(final IFile file, IProgressMonitor monitor) {
-        try {
-            // Parse controller is the "compiler" here; parses and reports errors
-            IParseController parseController = new PrefspecsParseController();
-
-            // Marker creator handles error messages from the parse controller (and
-            // uses the parse controller to get additional information about the errors)
-            MarkerCreator markerCreator = new MarkerCreator(file, parseController, 	PROBLEM_MARKER_ID);
-
-            // If we have a kind of parser that might be receptive, tell it
-            // what types of problem marker the builder will create
-            parseController.getAnnotationTypeInfo().addProblemMarkerType(getErrorMarkerID());
-            
-            // Need to tell the parse controller which file in which project to parse
-            // and also the message handler to which to report errors 
-            IProject project= file.getProject();
-    		ISourceProject sourceProject = null;
-        	try {
-        		sourceProject = ModelFactory.open(project);
-        	} catch (ModelException me){
-                System.err.println("PrefspecsBuilder.runParserForComplier(..):  Model exception:\n" + me.getMessage() + "\nReturning without parsing");
-                return;
-        	}	
-            parseController.initialize(file.getProjectRelativePath(), sourceProject, markerCreator);
-	
-            // Get file contents for parsing
-            String contents = BuilderUtils.extractContentsToString(file.getLocation().toString());
-        	
-            // Finally parse it
-            parseController.parse(contents, false, monitor);
-
-            doRefresh(file.getParent());
-        } catch (Exception e) {
-            getPlugin().writeErrorMsg(e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
 }
