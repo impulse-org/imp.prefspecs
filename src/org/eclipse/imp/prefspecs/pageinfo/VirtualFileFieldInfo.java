77c8c1cd76c107f83f2948114aeebeeeb4383e4f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-04-24 22:03:24 +0000

    Fixed relation of this class' ctors to the corresponding base class ctors.

--- src/org/eclipse/imp/prefspecs/pageinfo/VirtualFileFieldInfo.java
+++ src/org/eclipse/imp/prefspecs/pageinfo/VirtualFileFieldInfo.java
@@ -1,45 +1,29 @@
 /*******************************************************************************
-* Copyright (c) 2007 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ * Copyright (c) 2007 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+ *******************************************************************************/
 
-*******************************************************************************/
-
-/**
- * 
- */
 package org.eclipse.imp.prefspecs.pageinfo;
 
 /**
  * @author sutton
- *
  */
 public class VirtualFileFieldInfo extends VirtualStringFieldInfo {
+    public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name) {
+        super(parentPage, name);
+    }
 
-	
-	public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name) {
-		super(parentPage, name);
-	}
-	
-	public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name, String defValue) {
-		this(parentPage, name);
-		this.defaultValue = defValue;
-	}
+    public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name, String defValue) {
+        super(parentPage, name, defValue);
+    }
 
-	public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name,
-			String defValue, boolean hasSpecial, String special)
-	{
-		this(parentPage, name, defValue);
-		this.hasSpecialValue = hasSpecial;
-		this.specialValue = special;
-	}
-	
-
-	
-	
+    public VirtualFileFieldInfo(PreferencesPageInfo parentPage, String name, String defValue, boolean hasSpecial, String special) {
+        super(parentPage, name, defValue, hasSpecial, special);
+    }
 }
