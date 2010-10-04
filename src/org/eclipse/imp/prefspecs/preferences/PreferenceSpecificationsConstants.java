7cd671b914da102537d92d48a0760ce7102a215a
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-10-04 17:49:01 +0000

    Cosmetic changes from latest prefspecs compiler.

--- src/org/eclipse/imp/prefspecs/preferences/PreferenceSpecificationsConstants.java
+++ src/org/eclipse/imp/prefspecs/preferences/PreferenceSpecificationsConstants.java
@@ -1,3 +1,6 @@
+/******************************************/
+/* WARNING: GENERATED FILE - DO NOT EDIT! */
+/******************************************/
 package org.eclipse.imp.prefspecs.preferences;
 
 /**
@@ -6,17 +9,9 @@ package org.eclipse.imp.prefspecs.preferences;
  * The preferences service uses Strings as keys for preference values,
  * so Strings defined here are used here to designate preference fields.
  * These strings are generated automatically from a preferences specification.
- * Other constants may be defined here manually.
- *
  */
-
-
 public class PreferenceSpecificationsConstants {
-
 	public static final String P_TABWIDTH = "tabWidth";
-
 	public static final String P_SPACESFORTABS = "spacesForTabs";
-
 	public static final String P_EMITBUILDDIAGNOSTICS = "emitBuildDiagnostics";
-
 }
